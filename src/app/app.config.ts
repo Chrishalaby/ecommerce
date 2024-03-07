37d14bdd9eb25a9e5269c9a1a24b891a6d35303a
Chris <elhalabichristian@gmail.com>
2024-03-07 21:34:37 +0200

    Add @ngx-translate/http-loader dependency and update main.ts and app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,40 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import {
+  HttpBackend,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
 
+export function TranslateLoaderFactory(httpBackend: HttpBackend) {
+  // Note: Using HttpBackend to bypass HttpClient interceptors
+  // If you don't need to bypass interceptors, you can use HttpClient instead
+  return new TranslateHttpLoader(
+    new HttpClient(httpBackend),
+    './assets/i18n/',
+    '.json'
+  );
+}
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: TranslateLoaderFactory,
+    deps: [HttpBackend],
+  },
+});
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom([
+      HttpClientModule,
+      TranslateModule.forRoot(provideTranslation()),
+    ]),
+  ],
 };
